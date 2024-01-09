7f16ef8161888093fce12435ddf8238a563dab18
TimothyMcCann <timothysmccann@gmail.com>
2024-01-09 17:44:17 -0500

    completed lab

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About (blogData) {
+    const { name, image="https://via.placeholder.com/215", about } = blogData;
+    return (
+        <aside>
+            <img src={image} alt="blog logo"></img>
+            <p>{about}</p>
+        </aside>
+    )
+}
+
+export default About;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header (blogData) {
+    const { name, image, about } = blogData;
+
+    return (
+        <header>
+            <h1>{name}</h1>
+        </header>
+    )
+}
+
+export default Header;
